6432793b9591c8aac7eb7e49b1fb0caf5fcbb2de
dishant987 <tankdishant2003@gmail.com>
2024-09-01 15:45:36 +0530

    change

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -69,7 +69,7 @@ export default function Login() {
   const handleSubmit = async (values) => {
     setLoading(true);
     try {
-      const response = await axios.post(`${import.meta.env.VITE_BACKEND_URI}/api/users/signin`, values);
+      const response = await axios.post(`${import.meta.env.VITE_BACKEND_URI}/api/users/signin`, values,{withCredentials:true});
       if (response.data.statuscode === 200 && response.data.message === "Login SuccessFully") {
         toast.success(response.data.message);
         const expires = new Date();
